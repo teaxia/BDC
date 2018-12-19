c6d21401772376c9a7342c37cd0295a71db52b0b
teaxia <my@teaxia.com>
2018-12-19 16:00:56 +0800

    更新休闲娱乐排版

--- src/views/discovery/games.vue
+++ src/views/discovery/games.vue
@@ -2,14 +2,33 @@
 	<div class="games padding-footer" v-cloak>
         <x-header :left-options="{backText:$t('global.back')}" :title="$t('discovery.topmenu.ent')"></x-header>
         <div class="main-container">
-            <flexbox :gutter="0" wrap="wrap">
+            <!-- <flexbox :gutter="0" wrap="wrap">
                 <flexbox-item :span="1/4"  v-for="(v,index) in dataList" :key="index">
                     <div class="flex-div" @click="go(v.Url)">
                         <img :src="v.Img">
                         <div>{{v.Name}}</div>
                     </div>
                 </flexbox-item>
-            </flexbox>
+            </flexbox> -->
+            <v-grid v-for="(v,index) in dataList" :key="index" class="mr20">
+                <div class="pd-lb20" @click="go(v.Url)">
+                    <flexbox>
+                        <flexbox-item :span="3">
+                            <div class="dis-grid-img">
+                                <img :src="v.Img">
+                            </div>
+                        </flexbox-item>
+                        <flexbox-item :span="6">
+                            <div class="dis-grid-content">
+                                <div :class="{title:true,ellipsis1:true,link:v.Url}">{{v.Name}}</div>
+                            </div>
+                        </flexbox-item>
+                        <flexbox-item>
+                            <button class="btn btn-xs btn-round btn-success">开始游戏</button>
+                        </flexbox-item>
+                    </flexbox>
+                </div>
+            </v-grid>
 		</div>
 		<v-footer :isIndex="$route.meta.isIndex"></v-footer>
     </div>
@@ -62,11 +81,17 @@
 </script>
 
 <style scoped lang="scss">
-.flex-div{
-    display: flex;
-    align-items: center;
-    flex-wrap:wrap;
-    justify-content: center;
-    margin-bottom: 20px;
+// .flex-div{
+//     display: flex;
+//     align-items: center;
+//     flex-wrap:wrap;
+//     justify-content: center;
+//     margin-bottom: 20px;
+// }
+.btn{
+    font-size:24px;
+}
+.title{
+    font-size:32px;
 }
 </style>
