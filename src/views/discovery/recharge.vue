31a4192fd2dfacc15ab17e9c8d7272cf994d8aff
teaxia <my@teaxia.com>
2019-01-08 14:58:10 +0800

    流量售价截取8位

--- src/views/discovery/recharge.vue
+++ src/views/discovery/recharge.vue
@@ -46,7 +46,7 @@
                         <div>
                             <span>{{$t('discovery.recharge.price')}}：</span>
                             <!-- <span :class="v.off?'del':''">{{v.money/bdc}}BDC</span> -->
-                            <span>{{(v.money/bdc)*LLOff}}BDC</span> 
+                            <span>{{((v.money/bdc)*LLOff).toFixed(8)}}<br/>BDC</span> 
                         </div>
                         <!-- <div v-if="v.off" class="off">
                             {{$t('discovery.recharge.discount')}}：
@@ -282,13 +282,13 @@
                 this.current = 0;
                 if(type==1){
                     this.RMB     = this.phonecharges[0].money/this.bdc;
-                    this.Remakes = this.phonecharges[0].money+'CNY:'+this.phonecharges[0].money;
+                    this.Remakes = this.phonecharges[0].money+'CNY|'+this.phonecharges[0].money;
                 }else if(type==2){
                     this.RMB     = this.gprs[0].money/this.bdc;
-                    this.Remakes = this.gprs[0].money+'CNY:'+this.gprs[0].money;
+                    this.Remakes = this.gprs[0].money+'CNY|'+this.gprs[0].money;
                 }else if(type==3){
                     this.RMB     = this.oil[0].money/this.bdc;
-                    this.num     = this.oil[0].money+'CNY'
+                    this.num     = this.oil[0].money
                 }
             },
             // 充值金额选择
@@ -301,11 +301,11 @@
                         this.offprice   =  (money/this.bdc)*off
                     }
                     if(this.type==1){
-                        this.Remakes = this.phonecharges[index].money+'CNY:'+this.phonecharges[index].money
+                        this.Remakes = this.phonecharges[index].money+'CNY|'+this.phonecharges[index].money
                     }else if(this.type==2){
-                        this.Remakes = this.gprs[index].money+'CNY:'+this.gprs[index].money
+                        this.Remakes = this.gprs[index].money+'CNY|'+this.gprs[index].money
                     }else if(this.type==3){
-                        this.num  = this.oil[index].money+'CNY'
+                        this.num  = this.oil[index].money
                     }
                 }
                 
@@ -330,7 +330,7 @@
                     case '3':
                         this.coname = this.$refs.sect.value
                         this.class      = '油卡充值';
-                        this.Remakes    = '公司 :'+this.$refs.sect.value+' 油卡号 :'+this.oilcard;
+                        this.Remakes    = '公司 :'+this.$refs.sect.value+' 油卡号 :'+this.oilcard+' '+this.num+'CNY|'+this.num;
                         if(this.$refs.sect.value==''||this.mobile==''||this.oilcard==''){
                             this.$vux.toast.show({
                                 text: this.$t('discovery.recharge.error.full'),
@@ -424,7 +424,7 @@
 				if(data){
                     this.bdc     =  data[0].Money;
                     this.RMB     =  this.phonecharges[0].money/this.bdc;
-                    this.Remakes =  this.phonecharges[0].money+"CNY:"+this.phonecharges[0].money
+                    this.Remakes =  this.phonecharges[0].money+"CNY|"+this.phonecharges[0].money
                     this.offprice   =  (this.phonecharges[0].money/this.bdc)*0.5
 				}
             })
