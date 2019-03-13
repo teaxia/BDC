d44e61f29cc1740576531caeb196c8a1334b9051
teaxia <my@teaxia.com>
2019-03-13 17:05:08 +0800

    配置项增加有图标的币种

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,6 +2,17 @@
 全局配置文件
 */
 const jmCode = 'b2.@A%3dwa';                    //加密字段
+const currency = {
+    // 有图标的币种
+    BDC,
+    BTC,
+    ETH,
+    DASH,
+    USDT,
+    XRP,
+    BCH,
+    EOS
+}
 export default{
-    jmCode
+    jmCode,currency
 }
