0a14f2c627bd0cf7795c59e5b2b85b7555cf9328
teaxia <my@teaxia.com>
2019-05-05 12:43:24 +0800

    还原FastClick设置

--- src/main.js
+++ src/main.js
@@ -93,19 +93,13 @@ const router = new VueRouter({
 
 
 // 在IOS端会有300ms延迟的BUG，然后可能会产生键盘调用不灵敏的问题
+FastClick.attach(document.body)
 FastClick.prototype.onTouchEnd = function(event) {
   if(event.target.hasAttribute("type") && event.target.getAttribute("type") == "text") {
     event.preventDefault();　　　
     return false;　　
   }
 }
-FastClick.attach(document.body)
-// FastClick.prototype.onTouchEnd = function(event) {
-//   if(event.target.hasAttribute("type") && event.target.getAttribute("type") == "text") {
-//     event.preventDefault();　　　
-//     return false;　　
-//   }
-// }
 Vue.config.productionTip = false
 // 创建带有选项的 VueI18n 实例
 const i18n = new VueI18n({
